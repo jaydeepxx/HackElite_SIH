a960acf1e6c0b42086d5be9a98edf07381e27d73
Jaydeep <jaydeepmeena62@gmail.com>
2022-03-28 22:05:43 +0530

    added all seasons page

---
+++ Seasons/WinterSeason/season.js
@@ -0,0 +1,20 @@
+let nav = document.querySelector('nav');
+let dropdown = nav.querySelector('.dropdown');
+let dropdownToggle = nav.querySelector("[data-action='dropdown-toggle']");
+let navToggle = nav.querySelector("[data-action='nav-toggle']");
+
+dropdownToggle.addEventListener('click', () => {
+    if (dropdown.classList.contains('show')) {
+        dropdown.classList.remove('show');
+    } else {
+        dropdown.classList.add('show');
+    }
+})
+
+navToggle.addEventListener('click', () => {
+    if (nav.classList.contains('opened')) {
+        nav.classList.remove('opened');
+    } else {
+        nav.classList.add('opened');
+    }
+})
